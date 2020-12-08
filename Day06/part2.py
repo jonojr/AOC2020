100144e01970f7e2449c728013e52ec08749ef11
John <me@jonojr.tech>
2020-12-09 01:19:11 +1100

    Finished Day 6

---
+++ Day06/part2.py
@@ -0,0 +1,19 @@
+from functools import reduce
+
+
+def calculate_group_answers(group):
+    group_answers = group.split()
+    answer_sets = map(set, group_answers)
+    all_answers = reduce(lambda a, b: a & b, answer_sets)
+    return len(all_answers)
+
+
+if __name__ == "__main__":
+    with open("input.txt", 'r') as input_file:
+        answers = input_file.read().split("\n\n")
+
+    total = 0
+    for group in answers:
+        total += calculate_group_answers(group)
+
+    print(total)
