e395a64a80f8a317fcb072311ccb4233985fd2f1
John <me@jonojr.tech>
2020-12-21 21:36:24 +1100

    Remove unnecessary code from Day 10 Part 2

--- Day10/part2.py
+++ Day10/part2.py
@@ -1,4 +1,3 @@
-import sys
 from collections import defaultdict
 
 MEMOIZED_VALUES = defaultdict(lambda: 0)
@@ -7,7 +6,6 @@ MEMOIZED_VALUES[0] = 1
 
 
 if __name__ == "__main__":
-    sys.setrecursionlimit(9000)
     with open("input.txt", 'r') as input_file:
         values = [0]
         values.extend([int(value) for value in input_file.read().split("\n")])
